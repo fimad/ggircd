0953d295571b13c6269b969dcb2385c8af24e3be
Will Coster <willcoster@gmail.com>
2021-05-22 12:01:58 -0700

    Make LIST respect +sp
    Do not list channels either from global queries or by name if they are
    secret or private and the user is not in the channel.
    
    Issue #16

--- irc/handler_user_list_test.go
+++ irc/handler_user_list_test.go
@@ -85,5 +85,85 @@ func TestUserHandlerList(t *testing.T) {
 			state: newMockState().
 				withUser("nick"),
 		},
+		{
+			desc: "secret/private channels are hidden from outside",
+			in:   []message{cmdList},
+			wantNicks: map[string]mockConnection{
+				"nick": mockConnection{
+					messages: []message{
+						replyListEnd,
+					},
+				},
+			},
+			state: newMockState().
+				withChannel("#foo", "p", "topic").
+				withChannel("#bar", "s", "").
+				withUser("nick").
+				withUser("nick1", "#foo").
+				withUser("nick2", "#foo").
+				withUser("nick3", "#bar"),
+		},
+		{
+			desc: "secret/private channels are hidden from outside by name",
+			in:   []message{cmdList.withParams("#foo,#baz")},
+			wantNicks: map[string]mockConnection{
+				"nick": mockConnection{
+					messages: []message{
+						replyListEnd,
+					},
+				},
+			},
+			state: newMockState().
+				withChannel("#foo", "p", "topic").
+				withChannel("#bar", "s", "").
+				withUser("nick").
+				withUser("nick1", "#foo").
+				withUser("nick2", "#foo").
+				withUser("nick3", "#bar"),
+		},
+		{
+			desc: "secret channels are visible from inside",
+			in:   []message{cmdList},
+			wantNicks: map[string]mockConnection{
+				"nick": mockConnection{
+					messages: []message{
+						replyList.
+							withPrefix("name").
+							withParams("nick", "#foo", "3").
+							withTrailing("topic"),
+						replyListEnd,
+					},
+				},
+			},
+			state: newMockState().
+				withChannel("#foo", "s", "topic").
+				withChannel("#bar", "s", "").
+				withUser("nick", "#foo").
+				withUser("nick1", "#foo").
+				withUser("nick2", "#foo").
+				withUser("nick3", "#bar"),
+		},
+		{
+			desc: "private channels are visible from inside",
+			in:   []message{cmdList},
+			wantNicks: map[string]mockConnection{
+				"nick": mockConnection{
+					messages: []message{
+						replyList.
+							withPrefix("name").
+							withParams("nick", "#foo", "3").
+							withTrailing("topic"),
+						replyListEnd,
+					},
+				},
+			},
+			state: newMockState().
+				withChannel("#foo", "p", "topic").
+				withChannel("#bar", "p", "").
+				withUser("nick", "#foo").
+				withUser("nick1", "#foo").
+				withUser("nick2", "#foo").
+				withUser("nick3", "#bar"),
+		},
 	})
 }
